8d488979a756169c2bb099532ababa43863a4f17
Michal <michal.bainka@bayer.com>
2024-10-26 22:24:16 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const cors = require("cors");
+const router = express.Router();
+
+app.use(cors());
+
+router.get("/", (req, res) => {
+  res.send("App is running..");
+});
+
+router.get("/test", (req, res) => {
+  res.json([
+    {
+      id: "1",
+      name: "Test",
+    },
+  ]);
+});
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
